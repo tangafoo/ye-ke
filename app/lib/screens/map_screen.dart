dafb6f549979ea6cae96effdc7fdf0fe6391d12d
Tan Ga Foo <chevaliertann@gmail.com>
2026-08-31 11:35:37 +0800

    Add buttons to RadarView

--- app/lib/screens/map_screen.dart
+++ app/lib/screens/map_screen.dart
@@ -103,183 +103,190 @@ class _MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AnimatedContainer(
-          duration: const Duration(milliseconds: 250),
-          curve: Curves.easeInOut,
-          height: _radarOpen ? MediaQuery.sizeOf(context).height / 3 : 0,
-          width: double.infinity,
-          color: YeKe.bg1,
-          child: ClipRect(
-            child: SafeArea(
-              bottom: false,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        if (_rangeControlsOpened)
-                          Expanded(
-                            flex: 3,
-                            child: Text('Range', textAlign: TextAlign.center),
-                          )
-                        else ...[
-                          Expanded(
-                            child: Container(
-                              height: 20,
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: YeKe.bg1,
-                                border: BoxBorder.fromLTRB(
-                                  bottom: BorderSide(color: YeKe.night),
+    return ColoredBox(
+      color: YeKe.paint,
+      child: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 250),
+              curve: Curves.easeInOut,
+              height: _radarOpen ? MediaQuery.sizeOf(context).height / 3 : 0,
+              width: double.infinity,
+              child: ClipRect(
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          if (_rangeControlsOpened)
+                            Expanded(
+                              flex: 3,
+                              child: Text('Range', textAlign: TextAlign.center),
+                            )
+                          else ...[
+                            Expanded(
+                              child: Container(
+                                height: 20,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: YeKe.bg1,
+                                  border: BoxBorder.fromLTRB(
+                                    bottom: BorderSide(color: YeKe.night),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.newspaper,
+                                  color: YeKe.paper,
+                                  size: 42,
                                 ),
                               ),
-                              child: const Icon(
-                                Icons.newspaper,
-                                color: YeKe.paper,
-                                size: 42,
+                            ),
+
+                            Expanded(
+                              child: Container(
+                                height: 20,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: YeKe.scaley,
+                                  border: BoxBorder.fromLTRB(
+                                    bottom: BorderSide(color: YeKe.night),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.remove_red_eye,
+                                  color: YeKe.paper,
+                                  size: 42,
+                                ),
                               ),
                             ),
-                          ),
+                            Expanded(
+                              child: Container(
+                                height: 20,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: YeKe.scaley,
+                                  border: BoxBorder.fromLTRB(
+                                    bottom: BorderSide(color: YeKe.night),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.houseboat,
+                                  color: YeKe.paper,
+                                  size: 42,
+                                ),
+                              ),
+                            ),
+                          ],
 
                           Expanded(
-                            child: Container(
-                              height: 20,
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: YeKe.scaley,
-                                border: BoxBorder.fromLTRB(
-                                  bottom: BorderSide(color: YeKe.night),
+                            child: GestureDetector(
+                              onTap: () => setState(
+                                () => _rangeControlsOpened =
+                                    !_rangeControlsOpened,
+                              ),
+                              child: Container(
+                                height: 20,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: YeKe.scaley,
+                                  border: BoxBorder.fromLTRB(
+                                    bottom: BorderSide(color: YeKe.night),
+                                  ),
                                 ),
-                              ),
-                              child: const Icon(
-                                Icons.remove_red_eye,
-                                color: YeKe.paper,
-                                size: 42,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Container(
-                              height: 20,
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: YeKe.scaley,
-                                border: BoxBorder.fromLTRB(
-                                  bottom: BorderSide(color: YeKe.night),
+                                child: const Icon(
+                                  Icons.engineering,
+                                  color: YeKe.paper,
+                                  size: 42,
                                 ),
-                              ),
-                              child: const Icon(
-                                Icons.houseboat,
-                                color: YeKe.paper,
-                                size: 42,
                               ),
                             ),
                           ),
                         ],
-
-                        Expanded(
-                          child: GestureDetector(
-                            onTap: () => setState(
-                              () =>
-                                  _rangeControlsOpened = !_rangeControlsOpened,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Column(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            color: YeKe.cement,
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Scanner 2.0 ACAB ANTIFA',
+                              style: const TextStyle(
+                                fontFamily: 'Faculty',
+                                color: YeKe.bg1,
+                              ),
                             ),
+                          ),
+                          Expanded(
                             child: Container(
-                              height: 20,
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: YeKe.scaley,
-                                border: BoxBorder.fromLTRB(
-                                  bottom: BorderSide(color: YeKe.night),
+                              decoration: const BoxDecoration(
+                                color: YeKe.cement,
+                              ),
+                              padding: const EdgeInsets.fromLTRB(16, 0, 14, 16),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: YeKe.night,
+                                  borderRadius: BorderRadius.circular(16),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: YeKe.scaley,
+                                      offset: const Offset(6, 0),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              child: const Icon(
-                                Icons.engineering,
-                                color: YeKe.paper,
-                                size: 42,
+                                child: RadarView(),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 5,
-                    child: Column(
-                      children: [
-                        Container(
-                          width: double.infinity,
-                          color: YeKe.cement,
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Scanner 2.0 ACAB ANTIFA',
-                            style: const TextStyle(
-                              fontFamily: 'Faculty',
-                              color: YeKe.bg1,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            decoration: const BoxDecoration(color: YeKe.cement),
-                            padding: const EdgeInsets.fromLTRB(16, 0, 14, 16),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: YeKe.night,
-                                borderRadius: BorderRadius.circular(16),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: YeKe.scaley,
-                                    offset: const Offset(6, 0),
-                                  ),
-                                ],
-                              ),
-                              child: RadarView(),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () => setState(() => _radarOpen = !_radarOpen),
+              child: Container(
+                width: double.infinity,
+                color: YeKe.dusky,
+                padding: const EdgeInsets.symmetric(vertical: 2),
+                child: Icon(
+                  _radarOpen
+                      ? Icons.keyboard_arrow_up
+                      : Icons.keyboard_arrow_down,
+                  size: 16,
+                  color: YeKe.sub,
+                ),
+              ),
+            ),
+            Expanded(
+              child: FlutterMap(
+                mapController: _mapController,
+                options: MapOptions(
+                  onMapReady: _refresh,
+                  initialCenter: LatLng(3.1390, 101.6869),
+                  initialZoom: 12,
+                  onPositionChanged: _onPositionChanged,
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    userAgentPackageName: 'com.byakugan.byakugan',
                   ),
                 ],
               ),
             ),
-          ),
+          ],
         ),
-        GestureDetector(
-          onTap: () => setState(() => _radarOpen = !_radarOpen),
-          child: Container(
-            width: double.infinity,
-            color: YeKe.dusky,
-            padding: const EdgeInsets.symmetric(vertical: 2),
-            child: Icon(
-              _radarOpen ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down,
-              size: 16,
-              color: YeKe.sub,
-            ),
-          ),
-        ),
-        Expanded(
-          child: FlutterMap(
-            mapController: _mapController,
-            options: MapOptions(
-              onMapReady: _refresh,
-              initialCenter: LatLng(3.1390, 101.6869),
-              initialZoom: 12,
-              onPositionChanged: _onPositionChanged,
-            ),
-            children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.byakugan.byakugan',
-              ),
-            ],
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
